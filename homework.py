a8028869ff90d34e043df1f37da9258567b3e5dd
Michal <micham35@gmail.com>
2022-12-12 17:41:46 +0100

    homework 8 nypd, pytest testing

--- homework.py
+++ homework.py
@@ -1,9 +1,13 @@
 import argparse
 import json
 import os
+import tempfile
 from typing import List, Union
 
+import pytest
+
 current_dir = os.path.dirname(__file__)
+
 
 def take_from_list(li: list, indices: Union[int, List[int]]):
     """
@@ -32,6 +36,37 @@ def calculate(in_file: str, out_file: str):
 
     with open(out_file, 'w') as f_p:
         json.dump(result, f_p)
+
+
+@pytest.mark.parametrize('li,indices,res', [([], [], []), ([1, 2, 3], [], []), ([1, 2, 3], [0, 1, 2], [1, 2, 3])])
+def test_take_from_list(li: list, indices: Union[int, List[int]], res: List[int]):
+    assert take_from_list(li, indices) == res
+
+
+def test_exceptions_take_from_list():
+    with pytest.raises(IndexError):
+        take_from_list([1, 2, 3], [4])
+    with pytest.raises(ValueError):
+        take_from_list([1, 2, 3], 'ungabunga')
+
+
+@pytest.mark.parametrize(('content', 'res'),
+                         [('{"list": [21, 96], "indices": [1]}', '[96]'), ('{"list": [21, 96], "indices": []}', '[]')])
+def test_calculate(content: str, res: str, tmp_path):
+    content = '{"list": [21, 96], "indices": [1]}'
+    res = '[96]'
+
+    d = tmp_path / "sub"
+    d.mkdir()
+
+    input_file = d / "input.json"
+    input_file.write_text(content)
+
+    output_file = d / "output.json"
+
+    calculate(input_file, output_file)
+
+    assert output_file.read_text() == res
 
 
 if __name__ == '__main__':
