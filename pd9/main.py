0055bd6c17e7a832f42aa8daa159191f0f331e76
Michal <micham35@gmail.com>
2022-12-17 21:38:00 +0100

    homework 9 nypd

--- pd9/main.py
+++ pd9/main.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 
-
-if __name__ == '__main__':
-    arr = np.load('ex3_data.npy')
+def zad3(file):
+    arr = np.load(file)
     number_of_nan = np.sum(np.isnan(arr))
     number_of_rows_with_nan = np.sum(np.isnan(arr).any(axis=1))
     number_of_nan_in_columns = []
     for i in range(arr.shape[1]):
-        number_of_nan_in_columns += [np.sum(np.isnan(arr[:,i]))]
-    print(number_of_nan_in_columns)
+        number_of_nan_in_columns += [np.sum(np.isnan(arr[:, i]))]
+
+    arr = arr[~np.isnan(arr).any(axis=1)]
+
+    return {'Ile jest nan':number_of_nan, 'ile jest wierszy z nan':number_of_rows_with_nan, 'ile jest nan w kazdej kolumnie':number_of_nan_in_columns, 'maciersz po filtrowaniu':arr}
+
+
+if __name__ == '__main__':
+    print(zad3('ex3_data.npy'))
