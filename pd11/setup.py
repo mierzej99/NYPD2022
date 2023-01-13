f04db76ccb27a93e875c9ca4849662b87dfea7c9
Michal <micham35@gmail.com>
2023-01-13 17:28:44 +0100

    finished homework 11 abour creating pip installable package

---
+++ pd11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='homework11mm',
+    version='1.0',
+    description='Homework for python class',
+    author='Jan Kowalski',
+    install_requires=[
+        'numpy', 'os', 'json'
+    ],
+)
